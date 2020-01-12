be2adece1c65b33270e8f938faac8e483df0d04f
Deepakk-Vignesh <34776909+Deepakk-Vignesh@users.noreply.github.com>
2020-01-12 23:07:42 +0530

    Update 9jan.js

--- JS exercise/9jan.js
+++ JS exercise/9jan.js
@@ -1,28 +1,28 @@
 function getUnique(dataArray){
-    let arryCpy=new Set(dataArray);
-    arryCpy=Array.from(arryCpy);
-    for(var i=0;i<arryCpy.length;i++){
-        dataArray.splice(dataArray.indexOf(arryCpy[i]),1);
-        if(dataArray.indexOf(arryCpy[i])>-1){
-            arryCpy.splice(i--,1);
+    let dataArrayCopy=new Set(dataArray); //gets the set from dataArray
+    dataArrayCopy=Array.from(dataArrayCopy);  //converts the set object to array object
+    for(var i=0;i<dataArrayCopy.length;i++){  
+        dataArray.splice(dataArray.indexOf(dataArrayCopy[i]),1); //removes required element from dataArray
+        if(dataArray.indexOf(dataArrayCopy[i])>-1){  //checks whether the another same element is present in the same dataArray
+            dataArrayCopy.splice(i--,1);    //if present the element is removed from dataArrayCopy and shift due to removal is managed by decrementing the iterator
         }
     }
-    return arryCpy;
+    return dataArrayCopy;
 }
 
 function removeValues(dataArray,removeArray){
-    for(var i=0;i<removeArray.length;i++){
-        while(dataArray.indexOf(removeArray[i])>-1){
-        	dataArray.splice(dataArray.indexOf(removeArray[i]),1);
+    dataArray.foreach((removeElement)=>{
+        while(dataArray.indexOf(removeElement)>-1){
+        	dataArray.splice(dataArray.indexOf(removeElement),1);
         }
-    }
+    });
     return dataArray;
 }
 
 function getArray(inputStr){
 	inputStrArray=inputStr.split('\n');
 	for(var i=0;i<inputStrArray.length;i++){
-		inputStrArray[i]=Array.from((inputStrArray[i].split(',')));
+		inputStrArray[i]=inputStrArray[i].split(',');
 	}
 	return inputStrArray;
 }
