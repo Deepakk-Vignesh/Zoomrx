306d48c8eab91b55036111e12383102d0120c6bd
Deepakk-Vignesh <34776909+Deepakk-Vignesh@users.noreply.github.com>
2020-01-14 18:55:07 +0530

    Update 9jan.js

--- JS exercise/9jan.js
+++ JS exercise/9jan.js
@@ -1,22 +1,15 @@
 function getUnique(dataArray){
-    let dataArrayCopy=new Set(dataArray); //gets the set from dataArray
-    dataArrayCopy=Array.from(dataArrayCopy);  //converts the set object to array object
-    for(var i=0;i<dataArrayCopy.length;i++){  
-        dataArray.splice(dataArray.indexOf(dataArrayCopy[i]),1); //removes required element from dataArray
-        if(dataArray.indexOf(dataArrayCopy[i])>-1){  //checks whether the another same element is present in the same dataArray
-            dataArrayCopy.splice(i--,1);    //if present the element is removed from dataArrayCopy and shift due to removal is managed by decrementing the iterator
-        }
-    }
-    return dataArrayCopy;
+    let dataArraySet = new Set(dataArray);
+    dataArraySet = Array.from(dataArraySet);
+    return removeValues(dataArray,removeValues(dataArray,dataArraySet));
 }
 
 function removeValues(dataArray,removeArray){
-    removeArray.forEach((removeElement)=>{
-        while(dataArray.indexOf(removeElement)>-1){
-        	dataArray.splice(dataArray.indexOf(removeElement),1);
-        }
+    return dataArray.filter((element)=>{
+        if(removeArray.indexOf(element)<=-1){
+	   	return element;
+	   }
     });
-    return dataArray;
 }
 
 function getArray(inputStr){
